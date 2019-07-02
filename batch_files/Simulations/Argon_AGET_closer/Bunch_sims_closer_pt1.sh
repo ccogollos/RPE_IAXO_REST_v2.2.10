fce91ba83dd9e30e085f002fc638019e09919d53
ccogollos <cotri.ferm@gmail.com>
2019-07-02 14:17:14 +0200

    Preparing shell scripts for running several simulations

---
+++ batch_files/Simulations/Argon_AGET_closer/Bunch_sims_closer_pt1.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Co60FromCapBottom_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Cs137FromResTop_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/K40FromChip_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Ra228FromChip_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Th232FromCapBottom_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/U238FromChip_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Co60FromCapTop_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/K40FromResTop_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Th228FromCapBottom_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Th232FromCapTop_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/U238FromResTop_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Co60FromResTop_closer.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/IsotopeFromVolume_v2.rml
