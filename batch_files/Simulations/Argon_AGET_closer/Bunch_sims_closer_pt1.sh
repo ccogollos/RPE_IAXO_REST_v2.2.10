e748ee62bdfc41e5fc2a9d0d45d7df26e6688a26
ccogollos <cotri.ferm@gmail.com>
2019-07-03 10:21:43 +0200

    Modifying geometry. Bug: ChipVolume changed to ChipPhysVolume

--- batch_files/Simulations/Argon_AGET_closer/Bunch_sims_closer_pt1.sh
+++ batch_files/Simulations/Argon_AGET_closer/Bunch_sims_closer_pt1.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Co60FromCapBottom_closer.rml
-restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Cs137FromResTop_closer.rml
+#restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Co60FromCapBottom_closer.rml
+#restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Cs137FromResTop_closer.rml
 restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/K40FromChip_closer.rml
 restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Ra228FromChip_closer.rml
 restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_closer/Th232FromCapBottom_closer.rml
