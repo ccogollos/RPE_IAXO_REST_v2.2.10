a39057909a40279c1d64b0f4fc230e67ff8eaa43
Cristian <Doc@Cristians-MacBook-Pro.local>
2019-07-15 21:04:46 +0200

    Generating shell script for plots

---
+++ batch_files/Plots/Bunch_Plots.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Ra228Chip_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Co60CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th228CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Co60CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th228CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Co60ResTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th228Chip_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Cs137CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th228ResTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Cs137CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th232CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Cs137ResTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/K40CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th232CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/K40CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Th232ResTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/K40Chip_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/U238CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/K40ResTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/U238CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Ra226CapBottom_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/U238Chip_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Ra226CapTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/U238ResTop_HistoEnergy.rml
+restManager --c ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restPlots/Electronics_closer/Ra226ResTop_HistoEnergy.rml
