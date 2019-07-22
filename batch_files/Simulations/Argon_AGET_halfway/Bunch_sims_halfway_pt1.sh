063bc7287a76b8a898f0acedbb9322166a1280b5
ccogollos <cotri.ferm@gmail.com>
2019-07-22 12:57:51 +0200

    Preparing batch scripts for bunches of halfway electronics simulations

---
+++ batch_files/Simulations/Argon_AGET_halfway/Bunch_sims_halfway_pt1.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Co60FromCapBottom_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Cs137FromResTop_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/K40FromChip_halfway.rml
---
+++ batch_files/Simulations/Argon_AGET_halfway/Bunch_sims_halfway_pt3.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Ra228FromChip_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Th232FromCapBottom_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/U238FromChip_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Co60FromCapTop_halfway.rml
