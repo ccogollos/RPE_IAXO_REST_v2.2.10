f587a779f4fb25ab599f3f1988338f403aa494ea
ccogollos <cotri.ferm@gmail.com>
2019-07-30 12:46:23 +0200

    Reducing number of simulations on Th232FromCapBottom

---
+++ batch_files/Simulations/Argon_AGET_halfway/Bunch_sims_halfway_remaining.sh
@@ -0,0 +1,14 @@
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Co60FromCapTop_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Co60FromResTop_halfway.rml
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Cs137FromCapTop_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Cs137FromResTop_halfway.rml
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Th228FromCapBottom_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Th228FromResTop_halfway.rml
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/Th232FromResTop_halfway.rml
+
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/U238FromResTop_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/U238FromCapBottom_halfway.rml
+restG4 ~/RPE_IAXO_REST_v2.2.10/IAXO_AGET-REST/G4sims/restSims/AGET/Electronics_halfway/U238FromChip_halfway.rml
