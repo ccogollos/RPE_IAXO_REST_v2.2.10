c36cdf64a7bb4ade3aefac8ff639e470c5519163
ccogollos <cotri.ferm@gmail.com>
2019-08-27 17:24:24 +0200

    Increasing the amount of simulations

--- batch_files/Simulations/Argon_AGET_halfway/Halfway_CompleteSet/Simulate_Halfway_ptII.sh
+++ batch_files/Simulations/Argon_AGET_halfway/Halfway_CompleteSet/Simulate_Halfway_ptII.sh
@@ -11,7 +11,7 @@ do
 		then
 			for pos in "${position[@]}"
 			do
-				for (( i=1; i<=2; i++ ))
+				for (( i=1; i<=250; i++ ))
 				do
 					python temp_RESTsimfile_creation.py 100000 $iso $comp $pos $i
 					restfile="$iso"'From'"$comp""$pos"'_halfway_'"$i"'temp.rml'
@@ -20,7 +20,7 @@ do
 				done
 			done
 		else
-			for (( i=1; i<=2; i++ ))
+			for (( i=1; i<=250; i++ ))
 			do
 				python temp_RESTsimfile_creation.py 100000 $iso $comp TopCentre $i
 				restfile="$iso"'From'"$comp"'TopCentre_halfway_'"$i"'temp.rml'
--- batch_files/Simulations/Argon_AGET_halfway/Halfway_CompleteSet/Simulate_Halfway_ptV.sh
+++ batch_files/Simulations/Argon_AGET_halfway/Halfway_CompleteSet/Simulate_Halfway_ptV.sh
@@ -3,7 +3,7 @@ isotopesChip=("Th228" "U238")
 
 for iso in "${isotopesChip[@]}"
 do
-	for (( i=1; i<=2; i++ ))
+	for (( i=1; i<=250; i++ ))
 	do
 		python temp_RESTsimfile_creation.py 100000 $iso Chip None $i
 		restfile="$iso"'FromChip_halfway_'"$i"'temp.rml'
